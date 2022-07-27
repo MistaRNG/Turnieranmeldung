caf05bf07ce043425f79796e76ce4ec11a02619d
Robert Hobelsberger <11robert97@web.de>
2022-07-28 00:08:19 +0200

    typo fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
 return <div>
 
     <div className="disclaimer-content" >
-    <p className="payment-text">Bitte beachten Sie, dass die Teilnahmegebür nur vor Ort Bezahlt werden kann. </p>
+    <p className="payment-text">Bitte beachten Sie, dass die Teilnahmegebühr nur vor Ort Bezahlt werden kann. </p>
 
 
     <h4 className="disclaimer-heading">Disclaimer: Corona</h4>
